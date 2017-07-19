7023b8151ffb81375b8723feb060f7b8dd67aba6
nicky992 <anika.nietzer@t-online.de>
2017-07-19 15:39:17 +0200

    Fuege Struktur und Javadoc der Klassen des Codeprocessor-Packages hinzu

---
+++ Implementierung/visual-dfa/src/main/java/codeprocessor/Filter.java
@@ -0,0 +1,20 @@
+package codeprocessor;
+
+/**
+ * 
+ * @author Anika Nietzer Interface, that represents a filter for methods.
+ *
+ */
+public interface Filter {
+
+	/**
+	 * Filter for a given method, that decides whether the method should be filtered
+	 * out or not.
+	 * 
+	 * @param signature
+	 *            signature of the method
+	 * @return whether the method should be filtered out or not
+	 */
+	public boolean filter(String signature);
+
+}

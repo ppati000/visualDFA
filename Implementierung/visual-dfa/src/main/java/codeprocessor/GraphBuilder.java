45dd65a238bfce915cf4e7d701097d3156911ad7
nicky992 <anika.nietzer@t-online.de>
2017-07-19 15:39:17 +0200

    Fuege Struktur und Javadoc der Klassen des Codeprocessor-Packages hinzu

---
+++ Implementierung/visual-dfa/src/main/java/codeprocessor/GraphBuilder.java
@@ -0,0 +1,47 @@
+package codeprocessor;
+
+import java.util.List;
+import soot.toolkits.graph.BlockGraph;
+
+/**
+ * @author Anika Nietzer A {@code GraphBuilder} represents a unit, that
+ *         translates java-bytecode of one method to a Soot-SimpleBlockGraph.
+ */
+public class GraphBuilder {
+
+	/**
+	 * Creates a new {@code GraphBuilder} that works with the file present in
+	 * the package with packageName with the name className.
+	 * 
+	 * @param packageName
+	 *            name of the package
+	 * @param className
+	 *            name of the class
+	 */
+	public GraphBuilder(String packageName, String className) {
+
+	}
+
+	/**
+	 * Builds a {@code SimpleBlockGraph} of a method within the defined
+	 * class-file.
+	 * 
+	 * @param methodSignature
+	 *            name of the method
+	 * @return SimpleBlockGraph of the method
+	 */
+	public SimpleBlockGraph buildGraph(String methodSignature) {
+		return null;
+	}
+
+	/**
+	 * Returns a list of methods after the usage of filter.
+	 * 
+	 * @param filter
+	 *            filter, that decides which methods will be filtered out
+	 * @return list of all remaining methods
+	 */
+	public List<String> getMethods(Filter filter) {
+		return null;
+	}
+}

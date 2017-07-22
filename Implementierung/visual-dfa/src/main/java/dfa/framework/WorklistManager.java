30481f6a488754eeb4956df80dc23dd38be67e53
Sebastian Rauch <s.rauch94@googlemail.com>
2017-07-22 13:49:45 +0200

    Fuege stub fuer AnalysisLoader hinzu und implementiere WorklistManager.

---
+++ Implementierung/visual-dfa/src/main/java/dfa/framework/WorklistManager.java
@@ -0,0 +1,29 @@
+package dfa.framework;
+
+public class WorklistManager {
+
+    private static final String WL_NAME_NAIVE = "naive";
+    private static final String WL_NAME_RANDOM = "random";
+
+    private static WorklistManager singleInstance;
+
+    public static WorklistManager getInstance() {
+        if (singleInstance == null) {
+            singleInstance = new WorklistManager();
+        }
+
+        return singleInstance;
+    }
+
+    public Worklist getWorkist(String worklistName, SimpleBlockGraph blockGraph) {
+        switch (worklistName) {
+        case WL_NAME_NAIVE:
+            return new NaiveWorklist();
+        case WL_NAME_RANDOM:
+            return new RandomWorklist();
+        default:
+            throw new IllegalArgumentException("unknown worklist name: " + worklistName);
+        }
+    }
+
+}

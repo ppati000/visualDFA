47c0caabe0f8a3bbc188b292175d7cafaa1b0b76
nicky992 <anika.nietzer@t-online.de>
2017-07-23 15:36:15 +0200

    Add further implementation of controller package

--- Implementierung/visual-dfa/src/main/java/controller/AutoplayDriver.java
+++ Implementierung/visual-dfa/src/main/java/controller/AutoplayDriver.java
@@ -2,31 +2,46 @@ package controller;
 
 /**
  * 
- * @author Anika Nietzer 
- * 			An {@code AutoplayDriver} represents a unit, that is
- *         	responsible for the automatic performance of the animation of the
- *         	analysis-steps in a different thread.
+ * @author Anika Nietzer An {@code AutoplayDriver} represents a unit, that is
+ *         responsible for the automatic performance of the animation of the
+ *         analysis-steps in a different thread.
  *
  */
 public class AutoplayDriver implements Runnable {
 
-	/**
-	 * Performs the automatic replay of the analysis by using methods of the
-	 * {@code Controller).
-	 * 
-	 * @param controller
-	 *            instance of {@code Controller}
-	 */
-	public AutoplayDriver(Controller controller) {
+    private Controller controller;
 
-	}
+    /**
+     * Performs the automatic replay of the analysis by using methods of the
+     * {@code Controller).
+     * 
+     * @param controller
+     *            instance of {@code Controller}
+     */
+    public AutoplayDriver(Controller controller) {
+        if (controller == null) {
+            throw new IllegalArgumentException("controller must not be null");
+        }
+        this.controller = controller;
+    }
 
-	/**
-	 * Used during creation of a new thread, that performs the steps of the
-	 * analysis.
-	 */
-	public void run() {
-
-	}
+    @Override
+    /**
+     * Used during creation of a new thread, that performs the steps of the
+     * analysis.
+     */
+    public void run() {
+        boolean hasNextLine = true;
+        while (hasNextLine && this.controller.shouldAutoplayContinue()) {
+            try {
+                Thread.sleep((long) (controller.getDelay() * 1000));
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            hasNextLine = controller.nextLine();
+        }
+        this.controller.pause();
+    }
 
 }

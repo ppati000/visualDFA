9d396b510cd3dad81b16924adf3d732be2d40e96
MSchieber <MichaelDSchieber@gmx.de>
2017-08-05 16:48:37 +0200

    Add Method implementation to InputPanel

--- Implementierung/visual-dfa/src/main/java/gui/InputPanel.java
+++ Implementierung/visual-dfa/src/main/java/gui/InputPanel.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
@@ -14,7 +15,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 
 import java.awt.GridBagConstraints;
-import java.awt.Insets;
 import javax.swing.JComboBox;
 
 /**
@@ -29,6 +29,14 @@ public class InputPanel extends JPanel {
     private Controller ctrl;
     private CodeField codeField;
 
+    private JButton btnOpen;
+    private JButton btnSave;
+    private JLabel lblAnalyses;
+    private JComboBox<String> comboBox_Analyses;
+    private JLabel lblWorklists;
+    private JComboBox<String> comboBox_Worklists;
+    private JButton btnStartAnalysis;
+
     /**
      * Create the panel. Set the controller, so the ActionListeners can access
      * it.
@@ -41,73 +49,72 @@ public class InputPanel extends JPanel {
      */
     public InputPanel(Controller ctrl) {
         this.ctrl = ctrl;
-        
-        setBorder(new CompoundBorder(new BevelBorder(BevelBorder.RAISED, Colors.GREY_BORDER.getColor(), null, null, null), new EmptyBorder(5, 5, 5, 5)));
+
+        setBorder(
+                new CompoundBorder(new BevelBorder(BevelBorder.RAISED, Colors.GREY_BORDER.getColor(), null, null, null),
+                        new EmptyBorder(5, 5, 5, 5)));
         setBackground(Colors.BACKGROUND.getColor());
-        
+
         GridBagLayout gridBagLayout = new GridBagLayout();
-        gridBagLayout.columnWidths = new int[]{0, 0, 0};
-        gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
-        gridBagLayout.columnWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-        gridBagLayout.rowWeights = new double[]{0.5, 0.5, 0.5, 0.5, 0.0, 0.5, 0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 0.1, 0.1};
+        gridBagLayout.columnWidths = new int[] { 0, 0, 0 };
+        gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        gridBagLayout.columnWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+        gridBagLayout.rowWeights = new double[] { 0.5, 0.5, 0.5, 0.5, 0.0, 0.5, 0.0, 0.0, 0.5, 0.0, 0.0, 0.5, 0.1,
+                0.1 };
         setLayout(gridBagLayout);
-        
+
         JComponentDecorator jCompDecorator = new JComponentDecorator();
         JButtonDecorator jBuDecorator = new JButtonDecorator(jCompDecorator);
         JLabelDecorator jLaDecorator = new JLabelDecorator(jCompDecorator);
-        
+
         codeField = new CodeField(true);
         GridBagConstraints gbc_codeField = GridBagConstraintFactory.getStandardGridBagConstraints(0, 0, 2, 4);
         add(codeField, gbc_codeField);
-        
-        JButton btnOpen = new JButton();
+
+        btnOpen = new JButton();
         jBuDecorator.decorateIconButton(btnOpen, "icons/open-folder-outline.png", 0.2, null, "Open ...");
         btnOpen.setBackground(Colors.WHITE_BACKGROUND.getColor());
         btnOpen.setForeground(Colors.DARK_TEXT.getColor());
         GridBagConstraints gbc_btnOpen = GridBagConstraintFactory.getStandardGridBagConstraints(0, 4, 1, 1);
         gbc_btnOpen.insets.set(gbc_btnOpen.insets.top, gbc_btnOpen.insets.left, gbc_btnOpen.insets.bottom, 0);
         add(btnOpen, gbc_btnOpen);
-        
-        JButton btnSave = new JButton();
+
+        btnSave = new JButton();
         jBuDecorator.decorateIconButton(btnSave, "icons/save-file-option.png", 0.2, null, "Save ...");
         btnSave.setBackground(Colors.WHITE_BACKGROUND.getColor());
         btnSave.setForeground(Colors.DARK_TEXT.getColor());
         GridBagConstraints gbc_btnSave = GridBagConstraintFactory.getStandardGridBagConstraints(1, 4, 1, 1);
         gbc_btnSave.insets.set(gbc_btnSave.insets.top, 0, gbc_btnSave.insets.bottom, gbc_btnSave.insets.right);
         add(btnSave, gbc_btnSave);
-        
-        JLabel lblAnalyses = new JLabel();
+
+        lblAnalyses = new JLabel();
         jLaDecorator.decorateLabel(lblAnalyses, "Analysis");
         GridBagConstraints gbc_lblAnalyses = GridBagConstraintFactory.getStandardGridBagConstraints(0, 6, 2, 1);
         add(lblAnalyses, gbc_lblAnalyses);
-        
-        //TODO
-        //JComboBox comboBox_Analyses = new JComboBox(ctrl.getAnalysis().toArray());
-        JComboBox comboBox_Analyses = new JComboBox<String>();
+
+        comboBox_Analyses = new JComboBox<String>(ctrl.getAnalyses().toArray(new String[0]));
         GridBagConstraints gbc_comboBox_Analyses = GridBagConstraintFactory.getStandardGridBagConstraints(0, 7, 2, 1);
         gbc_comboBox_Analyses.fill = GridBagConstraints.HORIZONTAL;
         add(comboBox_Analyses, gbc_comboBox_Analyses);
-        
-        JLabel lblWorklists = new JLabel();
+
+        lblWorklists = new JLabel();
         jLaDecorator.decorateLabel(lblWorklists, "Worklist Algorithm");
         GridBagConstraints gbc_lblWorklists = GridBagConstraintFactory.getStandardGridBagConstraints(0, 9, 2, 1);
         add(lblWorklists, gbc_lblWorklists);
-        
-        //TODO
-        //JComboBox comboBox_Worklist = new JComboBox<String>(ctrl.getWorklists());
-        JComboBox comboBox_Worklist = new JComboBox<String>();
+
+        comboBox_Worklists = new JComboBox<String>(ctrl.getWorklists().toArray(new String[0]));
         GridBagConstraints gbc_comboBox_Worklist = GridBagConstraintFactory.getStandardGridBagConstraints(0, 10, 2, 1);
         gbc_comboBox_Worklist.fill = GridBagConstraints.HORIZONTAL;
-        add(comboBox_Worklist, gbc_comboBox_Worklist);
-        
-        JButton btnStartAnalysis = new JButton();
+        add(comboBox_Worklists, gbc_comboBox_Worklist);
+
+        btnStartAnalysis = new JButton();
         jBuDecorator.decorateButton(btnStartAnalysis, null, "Start Analysis");
         btnStartAnalysis.setBackground(Colors.GREEN_BACKGROUND.getColor());
+
         btnStartAnalysis.setFont(new Font("Trebuchet MS", Font.BOLD, 24));
         GridBagConstraints gbc_btnStartAnalysis = GridBagConstraintFactory.getStandardGridBagConstraints(0, 12, 2, 2);
         add(btnStartAnalysis, gbc_btnStartAnalysis);
-        
-        // TODO UI-Elements
+
     }
 
     /**
@@ -119,7 +126,15 @@ public class InputPanel extends JPanel {
      *            [false].
      */
     public void setActivated(boolean b) {
-        // TODO
+        // TODO disable Codefield
+
+        btnSave.setEnabled(b);
+        btnOpen.setEnabled(b);
+        lblAnalyses.setEnabled(b);
+        comboBox_Analyses.setEnabled(b);
+        lblWorklists.setEnabled(b);
+        comboBox_Worklists.setEnabled(b);
+        btnStartAnalysis.setEnabled(b);
     }
 
     /**
@@ -128,8 +143,8 @@ public class InputPanel extends JPanel {
      * @return The name of the data flow analysis.
      */
     public String getAnalysis() {
-        // TODO
-        return null;
+        String analysisName = (String) comboBox_Analyses.getSelectedItem();
+        return analysisName;
     }
 
     /**
@@ -139,8 +154,7 @@ public class InputPanel extends JPanel {
      *
      */
     public String getCode() {
-        // TODO
-        return null;
+        return codeField.getCode();
     }
 
     /**
@@ -149,8 +163,8 @@ public class InputPanel extends JPanel {
      * @return The name of the worklist-algorithm.
      */
     public String getWorklist() {
-        // TODO
-        return null;
+        String worklistName = (String) comboBox_Worklists.getSelectedItem();
+        return worklistName;
     }
 
     /**
@@ -163,4 +177,20 @@ public class InputPanel extends JPanel {
         return false;
     }
 
+    /**
+     * Implementation of an ActionListener which informs the controller, when
+     * the StartAnalysis button has been pressed.
+     * 
+     * @author Michael
+     *
+     * @see ActionListener
+     */
+    private class StartAnalysisListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            ctrl.startAnalysis();
+        }
+
+    }
 }

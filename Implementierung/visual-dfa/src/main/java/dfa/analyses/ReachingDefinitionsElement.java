8158ceaf4782c6217c9fe92a33d696539156cbf1
hirnlos23 <nils.jessen@hotmail.de>
2017-08-13 18:23:56 +0200

    Implement ConstantBits Transition and partially StringRepresentation

--- Implementierung/visual-dfa/src/main/java/dfa/analyses/ReachingDefinitionsElement.java
+++ Implementierung/visual-dfa/src/main/java/dfa/analyses/ReachingDefinitionsElement.java
@@ -5,6 +5,65 @@ import java.util.Map;
 import java.util.Objects;
 
 import dfa.analyses.ReachingDefinitionsElement.Definition;
+import dfa.framework.UnsupportedValueException;
+import soot.BooleanType;
+import soot.ByteType;
+import soot.CharType;
+import soot.IntType;
+import soot.Local;
+import soot.LongType;
+import soot.ShortType;
+import soot.Type;
+import soot.Value;
+import soot.jimple.AddExpr;
+import soot.jimple.AndExpr;
+import soot.jimple.ArrayRef;
+import soot.jimple.BinopExpr;
+import soot.jimple.CastExpr;
+import soot.jimple.CaughtExceptionRef;
+import soot.jimple.ClassConstant;
+import soot.jimple.CmpExpr;
+import soot.jimple.CmpgExpr;
+import soot.jimple.CmplExpr;
+import soot.jimple.ConstantSwitch;
+import soot.jimple.DivExpr;
+import soot.jimple.DoubleConstant;
+import soot.jimple.DynamicInvokeExpr;
+import soot.jimple.EqExpr;
+import soot.jimple.FloatConstant;
+import soot.jimple.GeExpr;
+import soot.jimple.GtExpr;
+import soot.jimple.InstanceFieldRef;
+import soot.jimple.InstanceOfExpr;
+import soot.jimple.IntConstant;
+import soot.jimple.InterfaceInvokeExpr;
+import soot.jimple.JimpleValueSwitch;
+import soot.jimple.LeExpr;
+import soot.jimple.LengthExpr;
+import soot.jimple.LongConstant;
+import soot.jimple.LtExpr;
+import soot.jimple.MethodHandle;
+import soot.jimple.MulExpr;
+import soot.jimple.NeExpr;
+import soot.jimple.NegExpr;
+import soot.jimple.NewArrayExpr;
+import soot.jimple.NewExpr;
+import soot.jimple.NewMultiArrayExpr;
+import soot.jimple.NullConstant;
+import soot.jimple.OrExpr;
+import soot.jimple.ParameterRef;
+import soot.jimple.RemExpr;
+import soot.jimple.ShlExpr;
+import soot.jimple.ShrExpr;
+import soot.jimple.SpecialInvokeExpr;
+import soot.jimple.StaticFieldRef;
+import soot.jimple.StaticInvokeExpr;
+import soot.jimple.StringConstant;
+import soot.jimple.SubExpr;
+import soot.jimple.ThisRef;
+import soot.jimple.UshrExpr;
+import soot.jimple.VirtualInvokeExpr;
+import soot.jimple.XorExpr;
 import soot.jimple.internal.JimpleLocal;
 
 /**
@@ -62,7 +121,7 @@ public class ReachingDefinitionsElement extends LocalMapElement<Definition> {
     }
 
     @Override
-    public LocalMapElement clone() {
+    public LocalMapElement<Definition> clone() {
         return new ReachingDefinitionsElement(getLocalMap());
     }
 
@@ -95,6 +154,10 @@ public class ReachingDefinitionsElement extends LocalMapElement<Definition> {
         private static final Definition BOTTOM = new Definition(DefinitionType.BOTTOM);
         private static final Definition TOP = new Definition(DefinitionType.TOP);
 
+        // TODO use proper symbols (after testing)
+        private static final String BOTTOM_SYMBOL = /* "\u22A5" */ "B";
+        private static final String TOP_SYMBOL = /* "\u22A4" */ "T";
+
         private soot.Value val;
         private DefinitionType type;
 
@@ -120,6 +183,350 @@ public class ReachingDefinitionsElement extends LocalMapElement<Definition> {
 
         public boolean isActualDefinition() {
             return (this.type == DefinitionType.DEFINITION);
+        }
+
+        @Override
+        public String toString() {
+            if (this.type == DefinitionType.TOP) {
+                return TOP_SYMBOL;
+            } else if (this.type == DefinitionType.BOTTOM) {
+                return BOTTOM_SYMBOL;
+            } else {
+                StringRepresentation valueSwitch = new StringRepresentation(this);
+                val.apply(valueSwitch);
+                return valueSwitch.getResult();
+            }
+        }
+    }
+
+    /**
+     * @author Nils Jessen
+     * 
+     *         A {@code StringRepresentation} evaluates {@code JimpleValue}s (e. g. {@code soot.jimple.Expr}).
+     */
+    static class StringRepresentation implements JimpleValueSwitch {
+
+        private Definition inputDefinition;
+
+        private String result;
+
+        /**
+         * Creates a new {@code StringRepresentation} with the given input-{@code ReachingDefinitionsElement}.
+         * 
+         * @param inputDefinition
+         *        the input for this {@code StringRepresentaton}, on which the evaluation is based on
+         */
+        public StringRepresentation(Definition inputDefinition) {
+            this.inputDefinition = inputDefinition;
+        }
+
+        /**
+         * Returns the result.
+         * 
+         * @return the result
+         */
+        public String getResult() {
+            return result;
+        }
+
+        // ConstantSwitch
+
+        @Override
+        public void caseClassConstant(ClassConstant c) {
+            // ignore
+        }
+
+        @Override
+        public void caseDoubleConstant(DoubleConstant c) {
+            result = Double.toString(c.value);
+        }
+
+        @Override
+        public void caseFloatConstant(FloatConstant c) {
+            result = Float.toString(c.value);
+        }
+
+        @Override
+        public void caseIntConstant(IntConstant c) {
+            result = Integer.toString(c.value);
+        }
+
+        @Override
+        public void caseLongConstant(LongConstant c) {
+            result = Long.toString(c.value);
+        }
+
+        @Override
+        public void caseMethodHandle(MethodHandle c) {
+            result = c.toString();
+            // TODO Documentation?
+        }
+
+        @Override
+        public void caseNullConstant(NullConstant c) {
+            result = "null";
+        }
+
+        @Override
+        public void caseStringConstant(StringConstant c) {
+            result = c.value;
+        }
+
+        // ExprSwitch
+
+        /*
+         * constant # BOTTON = BOTTOM for # in {+,-,*,/,%,&,|,^,<<,<<<,>>}
+         */
+
+        private String[] retrieveStringPair(soot.jimple.BinopExpr expr) {
+            Value op1 = expr.getOp1();
+            Value op2 = expr.getOp2();
+
+            StringRepresentation valueSwitch1 = new StringRepresentation(inputDefinition);
+            op1.apply(valueSwitch1);
+            StringRepresentation valueSwitch2 = new StringRepresentation(inputDefinition);
+            op2.apply(valueSwitch2);
+
+            String[] stringPair = { valueSwitch1.getResult(), valueSwitch2.getResult() };
+            return stringPair;
+        }
+
+        @Override
+        public void caseAddExpr(AddExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") + (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseSubExpr(SubExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") - (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseMulExpr(MulExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") * (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseDivExpr(DivExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") / (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseRemExpr(RemExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") % (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseNegExpr(NegExpr expr) {
+            Value op = expr.getOp();
+            StringRepresentation valueSwitch = new StringRepresentation(inputDefinition);
+            op.apply(valueSwitch);
+            result = "(-1) * " + valueSwitch.getResult();
+        }
+
+        @Override
+        public void caseAndExpr(AndExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") & (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseOrExpr(OrExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") | (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseXorExpr(XorExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") ^ (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseShlExpr(ShlExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") << (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseShrExpr(ShrExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") >> (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseUshrExpr(UshrExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") >>> (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseCmplExpr(CmplExpr expr) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void caseCmpgExpr(CmpgExpr expr) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void caseCmpExpr(CmpExpr expr) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void caseCastExpr(CastExpr expr) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void caseEqExpr(EqExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") == (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseGeExpr(GeExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") >= (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseGtExpr(GtExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") > (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseLeExpr(LeExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") <= (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseLtExpr(LtExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") < (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseNeExpr(NeExpr expr) {
+            String[] opString = retrieveStringPair(expr);
+            result = "(" + opString[0] + ") != (" + opString[1] + ")";
+        }
+
+        @Override
+        public void caseInstanceOfExpr(InstanceOfExpr expr) {
+            Value op = expr.getOp();
+            StringRepresentation valueSwitch = new StringRepresentation(inputDefinition);
+            op.apply(valueSwitch);
+            result = "instanceof " + valueSwitch.getResult();
+        }
+
+        @Override
+        public void caseInterfaceInvokeExpr(InterfaceInvokeExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseDynamicInvokeExpr(DynamicInvokeExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseLengthExpr(LengthExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseNewArrayExpr(NewArrayExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseNewExpr(NewExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseNewMultiArrayExpr(NewMultiArrayExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseSpecialInvokeExpr(SpecialInvokeExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseStaticInvokeExpr(StaticInvokeExpr expr) {
+            // TODO implement Expression
+        }
+
+        @Override
+        public void caseVirtualInvokeExpr(VirtualInvokeExpr expr) {
+            // TODO implement Expression
+        }
+
+        // RefSwitch
+
+        @Override
+        public void caseArrayRef(ArrayRef ref) {
+            result = ref.getBase().toString();
+            // TODO implement correctly
+        }
+
+        @Override
+        public void caseCaughtExceptionRef(CaughtExceptionRef ref) {
+            throw new UnsupportedValueException("CaughtExceptionRef", ref);
+        }
+
+        @Override
+        public void caseInstanceFieldRef(InstanceFieldRef ref) {
+            result = ref.getBase().toString();
+            // TODO implement correctly
+        }
+
+        @Override
+        public void caseParameterRef(ParameterRef ref) {
+            result = ref.toString();
+            // TODO implement correctly
+        }
+
+        @Override
+        public void caseStaticFieldRef(StaticFieldRef ref) {
+            result = ref.getField().toString();
+            // TODO implement correctly
+        }
+
+        @Override
+        public void caseThisRef(ThisRef ref) {
+            result = ref.toString();
+            // TODO implement correctly
+        }
+
+        // JimpleValueSwitch
+
+        @Override
+        public void caseLocal(Local local) {
+            if (!(local instanceof JimpleLocal)) {
+                throw new IllegalStateException("No Jimple local!");
+            }
+            result = ((JimpleLocal) local).getName();
+        }
+
+        @Override
+        public void defaultCase(Object arg0) {
+            assert false : "No soot Value - You fucked up!";
         }
     }
 
